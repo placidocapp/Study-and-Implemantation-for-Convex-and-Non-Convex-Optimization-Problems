a412ecd2d369855f0af1ea5af756d5dc0d81deeb
Placido Capp Campos <placidoccampos@gmail.com>
2018-06-17 13:22:14 -0300

    Add the cholesky factorization to implement a new newton methd with hessian modificantion

--- Line Search/BacktrackingWithModifiedHessian.m
+++ Line Search/BacktrackingWithModifiedHessian.m
@@ -25,7 +25,7 @@ maxIter = 100;
 %Size (Selects size based on the gradient size)
 n = size(df(zeros(10,10)),1);
 
-%% Backtraking Algorithm
+%% Backtraking Algorithm (Modified Hessian with 2-norm)
 
 %Use this to find de derivative of a new function
 % syms x;
@@ -34,8 +34,8 @@ n = size(df(zeros(10,10)),1);
 
 %Inicialization of x
 x = zeros(maxIter,n);
-% x(1,:) = randn(1,n);
-x(1,:) = 0.333510833065806;
+x(1,:) = randn(1,n);
+% x(1,:) = 0.333510833065806;
 
 %Backtraking loop
 for k = 1:maxIter
@@ -47,7 +47,7 @@ for k = 1:maxIter
         lambda = min(eig(H));
         H = H + eye*(-lambda+0.1);
     end
-    H
+    
     d = -inv(H)*df(x(k,:));  %Choose the direction as an steepest gradient descent
     alpha = 1;      %Step size, firs gess
     
@@ -59,9 +59,46 @@ for k = 1:maxIter
     %Find the next point
     x(k+1,:) = (x(k,:)' + alpha*d)';
 end
-out = [x, x];
-sol = [f(x(end,:)') f(x(end,:)')];
-%% Backtraking Algorithm
+out = [x, x, x];
+sol = [f(x(end,:)') f(x(end,:))' f(x(end,:))'];
+
+%% Backtraking Algorithm (Modified Hessian with cholesky factorization)
+
+%Use this to find de derivative of a new function
+% syms x;
+% f = f(x);
+% df = diff(f);
+
+%Inicialization of x
+x = zeros(maxIter,n);
+x(1,:) = randn(1,n);
+% x(1,:) = 0.333510833065806;
+
+%Backtraking loop
+for k = 1:maxIter
+    
+    %Calculates the hessian, if some eigenvalue of H is negative than
+    %change the hessian for itself plus eye*|most negative eigenvalue + lambda|
+    H = d2f(x(k,:));
+    if sum(eig(H) < 0) > 0
+        [L,D] = mcfac(H);
+        H = L*D*L';
+    end
+    
+    d = -inv(H)*df(x(k,:));  %Choose the direction as an steepest gradient descent
+    alpha = 1;      %Step size, firs gess
+    
+    %Reduce the step until it satisfies the Armijo condition
+    while f(x(k,:)' + alpha*d) > f(x(k,:)') + c1*alpha*df(x(k,:))'*d 
+        alpha = ro*alpha;
+    end
+    
+    %Find the next point
+    x(k+1,:) = (x(k,:)' + alpha*d)';
+end
+out(:,2) = x
+sol(:,2) = f(x(end,:)')
+%% Backtraking Algorithm (Steepest Descent)
 
 %Use this to find de derivative of a new function
 % syms x;
@@ -83,13 +120,19 @@ for k = 1:maxIter
 end
     
 disp('Compare the steepest gradient descent vs newton method:')
-out(:,2) = x
-sol(:,2) = f(x(end,:)')
+out(:,3) = x
+sol(:,3) = f(x(end,:)')
     
 %Plot the error
-error = abs(out - mean(sol));
-plot(out(:,1), 'g'), hold on, plot(out(:,2), 'r')
-legend('Newton Method','Steepest Gradient Descent')
+error = zeros(maxIter,2);
+for j = 1:3
+    for i = 1:maxIter
+        error(i,j) = abs(f(out(i,j)) - mean(sol));
+    end
+end
+
+plot(error(:,1), 'b'), hold on, plot(error(:,2), 'g'), plot(error(:,3), 'r')
+legend('Newton Method With 2-norm','Newton Method With Cholesky','Steepest Gradient Descent')
 title('Error of Steepest Gradient Descent vs Newton Method')
 % axis([0 10 0 5])
     
